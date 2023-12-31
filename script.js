6eed45d85c44acb6c7fa69cc1f456d5b65748c54
Reptudn <densetsu467@gmail.com>
2023-12-31 19:34:34 +0100

    updates

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-alert('Under construction');
+
+var board = [5][5]; //defaulting size to 5x5 for now
+var quests = [];
+
+class Quest {
+	constructor(task, description, difficulty) {
+		this.task = task;
+		this.description = description;
+		this.difficulty = difficulty;
+	}
+	done = false;
+}
+
+fetch('./quests.json')
+	.then(response => response.json())
+	.then(data => {
+		quests = data.map(questData => new Quest(questData.task, questData.description, questData.difficulty));
+		console.log(quests);
+	})
+	.catch(error => console.error('Error fetching quests:', error));
+
+function questAlreadyOnBoard(quest) {
+	for (let i = 0; i < quests.length; i++)
+		if (quests[i].task == quest.task) return true;
+	return false;
+}
+
+function getRandomQuest(quests) {
+	let quest = quests[Math.floor(Math.random() * quests.length)];
+	if (questAlreadyOnBoard(quest)) {
+		console.log("Quest already on board, getting another...");
+		getRandomQuest(quests);
+	} else {
+		console.log(quest);
+		return quest;
+	}
+}
+
+function generateBoard(board) {
+	for (let i = 0; i < board.length; i++)
+		for (let j = 0; j < board[i].length; j++)
+			board[i][j] = getRandomQuest(quests);
+}
+
+generateBoard(board);
