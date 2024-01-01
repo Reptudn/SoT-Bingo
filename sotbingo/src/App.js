c6d5e24c11b94eb92fcb72959b38f594b94d53f3
Reptudn <densetsu467@gmail.com>
2024-01-01 19:51:45 +0100

    remove routes

--- sotbingo/src/App.js
+++ sotbingo/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Board from './components/Board.js';
 import Data from './assets/quests.json';
-import About from './pages/About.js';
 import './App.css';
 
 function App() {
@@ -14,14 +13,9 @@ function App() {
   }, []);
 
   return (
-    <Router basename='/SoT-Bingo'>
-      <Routes>
-        <Route path="/" element={
-          <Board quests={quests}/>
-        } />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <div class="App">
+      <Board quests={quests}/>
+    </div>
   );
 }
 
